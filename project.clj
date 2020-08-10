34d6579f48d78a5b18123a66320167ddd1599e32
Shtanglitza <shtanglitza@gmail.com>
2020-08-10 21:17:52 +0200

    Added auto deploy for lein and git action

--- project.clj
+++ project.clj
@@ -3,5 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[lein-tools-deps "0.4.5"]
+            [s3-wagon-private "1.3.4"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns ifc-tools-clj.core})
+  :lein-tools-deps/config {:config-files [:project]
+                           :resolve-aliases [:test]}
+  :repl-options {:init-ns ifc-tools-clj.step.core}
+  :repositories [["shtanglitza" {:url "s3p://shtanglitza/clojure"
+                                 :username :env/AWS_ACCESS_KEY_ID
+                                 :passphrase :env/AWS_SECRET_ACCESS_KEY}]])
